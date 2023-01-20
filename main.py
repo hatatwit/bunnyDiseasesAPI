14a59fcb745b425d6dfb06aadde12d2741366f7e
hatatwit <hatrang061299@yahoo.com>
2023-01-20 09:40:42 -0500

    Fix CORS error

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import pickle
 
 # Initializing FastAPI server
 app = FastAPI()
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Loading up the Trained Model
 model = pickle.load(open("./diseases_trained_model.sav", "rb"))
